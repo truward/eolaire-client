3f65481841d608ac2f1ea57d9fac28f5d8a705b4
Alexander Shabanov <avshabanov@gmail.com>
2015-08-11 21:01:41 -0700

    Adding get operations for item profiles and relations

--- eolaire-model/src/main/java/com/truward/orion/eolaire/model/EolaireRestService.java
+++ eolaire-model/src/main/java/com/truward/orion/eolaire/model/EolaireRestService.java
@@ -2,6 +2,8 @@ package com.truward.orion.eolaire.model;
 
 import org.springframework.web.bind.annotation.*;
 
+import static com.truward.orion.eolaire.model.EolaireModel.*;
+
 /**
  * Defines a contract for major RESTful operations on user service.
  *
@@ -9,11 +11,31 @@ import org.springframework.web.bind.annotation.*;
  */
 public interface EolaireRestService {
 
-  @RequestMapping(value = "/item/{id}", method = RequestMethod.GET)
+  @RequestMapping(value = "/item/entry/{id}", method = RequestMethod.GET)
   @ResponseBody
-  EolaireModel.Item getItemById(@PathVariable("id") long id);
+  Item getItemById(@PathVariable("id") long id);
 
   @RequestMapping(value = "/item/list", method = RequestMethod.POST)
   @ResponseBody
-  EolaireModel.GetItemsResponse getItemsByIds(@RequestBody EolaireModel.GetItemsRequest request);
+  GetItemsResponse getItemsByIds(@RequestBody GetItemsRequest request);
+
+  @RequestMapping(value = "/item/profile/{id}", method = RequestMethod.GET)
+  @ResponseBody
+  GetItemProfileResponse getItemProfile(@PathVariable("id") long id);
+
+  @RequestMapping(value = "/entity", method = RequestMethod.GET)
+  @ResponseBody
+  GetEntityTypeResponse getEntityTypeByName(@RequestParam("name") String name);
+
+  @RequestMapping(value = "/entity/list", method = RequestMethod.POST)
+  @ResponseBody
+  GetAllEntityTypesResponse getAllEntities(@RequestBody GetAllEntityTypesRequest request);
+
+  @RequestMapping(value = "/item/query/by-type", method = RequestMethod.POST)
+  @ResponseBody
+  GetItemByTypeResponse getItemByType(@RequestBody GetItemByTypeRequest request);
+
+  @RequestMapping(value = "/item/query/by-rel", method = RequestMethod.POST)
+  @ResponseBody
+  GetItemByRelationResponse getItemByRelation(@RequestBody GetItemByRelationRequest request);
 }
